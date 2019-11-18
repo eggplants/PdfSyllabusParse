33a4f38eb19d83087603bec36cc2156389e8d65e
haruna <s1811528@s.tsukuba.ac.jp>
2019-11-18 13:58:14 +0900

    wget fix

--- wgetunko.sh
+++ wgetunko.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 curl -s https://klis.tsukuba.ac.jp/school_affairs.html|
-tr \> \\n|
-egrep -o "assets/files/.*\.pdf"|while read i;do wget "https://klis.tsukuba.ac.jp/${i}";done
+tr \> \\n|egrep -o "assets/files/.*\.pdf"|
+sed 's_^_https://klis.tsukuba.ac.jp_g'|
+while read i;do wget $i;done
 
+curl -s "https://klis.tsukuba.ac.jp/syllabus.html"|
+egrep -o "assets\/files\/syllabus.*pdf"|sort|uniq|
+sed 's_^_https://klis.tsukuba.ac.jp_g'|
+while read i;do wget $i;done
